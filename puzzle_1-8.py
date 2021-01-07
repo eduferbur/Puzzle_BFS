79d25fc269000c1e584497f01e65547d5c13b439
eduferbur <eduferbur@gmail.com>
2021-01-07 21:13:30 +0100

    Funcionando

--- puzzle_1-8.py
+++ puzzle_1-8.py
@@ -1,5 +1,6 @@
 from nodo import Nodo
 import numpy as np
+import copy
 
 # Puzle Lineal con busqueda en amplitud
 def puzzle_BFS(estado_inicial, solucion):
@@ -9,9 +10,13 @@ def puzzle_BFS(estado_inicial, solucion):
     nodo_inicial = Nodo(estado_inicial)
     nodos_frontera.append(nodo_inicial)
     while (not solucionado) and len(nodos_frontera) != 0:
-        nodo = nodos_frontera.pop(0)
+        # nodo = nodos_frontera.pop(0)
+        # # extraer nodo y añadirlo a visitados
+        # nodos_visitados.append(nodo)
+
+        nodo = nodos_frontera[0]
         # extraer nodo y añadirlo a visitados
-        nodos_visitados.append(nodo)
+        nodos_visitados.append(nodos_frontera.pop(0))
 
         # Hasta aquí todo es igual que en el ejemplo.
         compara = nodo.get_datos() == solucion # Como es un array de Numpy, así lo comparamos
@@ -21,18 +26,22 @@ def puzzle_BFS(estado_inicial, solucion):
             return nodo
         else:
             # expandir nodos hijo
-            dato_nodo = nodo.get_datos()
-            posibles_direcciones = ["D", "I", "U", "A"] # Movimientos de la función intercambio_array, ver en la función
+            posibles_direcciones = ["D", "I", "U", "A"] # Posibles movimientos de la función intercambio_array, ver en la función
+            lista_hijos = []    # Aquí vamos añadiendo los hijos que vamos creando.
             for direccion in posibles_direcciones:
+                dato_nodo = copy.copy(nodo.get_datos())
                 '''Hago un try porque le voy a pedir que haga todas las posibles combinaciones, 
                 Si me salgo del índice de la matrix, dará un indexerror, pero seguirá la con los siguientes movimientos'''
                 try:
-                    hijo = intercambio_array(dato_nodo, direccion)    # CRISTIAN AQUÍ :'( Cambia nodos_visitados[0]!! Por que!!??
-                    hijo_nodo = Nodo(hijo)
-                    if not hijo_nodo.en_lista(nodos_visitados) and not hijo_nodo.en_lista(nodos_frontera):
-                        nodos_frontera.append(hijo_nodo)
+                    un_hijo = intercambio_array(dato_nodo, direccion)
+                    hijo = copy.copy(Nodo(un_hijo))
+                    lista_hijos.append(hijo)
+                    if not hijo.en_lista(nodos_visitados) and not hijo.en_lista(nodos_frontera):
+                        nodos_frontera.append(hijo)
+
                 except:
                     pass
+            nodo.set_hijos(lista_hijos)    # Añadimos la lista de hijos al nodo padre.
 
 
 def intercambio_array(array, direccion):
@@ -59,18 +68,25 @@ def intercambio_array(array, direccion):
 
 
 if __name__ == "__main__":
-    estado_inicial = np.array([[2, 8, 3], [1, 6, 4], [7, 0, 5]])
+    estado_inicial = np.array([[8, 3, 2], [1, 6, 4], [7, 0, 5]])
     solucion = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
     nodo_solucion = puzzle_BFS(estado_inicial, solucion)
-    # mostrar resultado
-    resultado = []
-    nodo = nodo_solucion
-    compara = nodo.get_padre() == None
-    while compara != True:
-        resultado.append(nodo.get_datos())
-        nodo = nodo.get_padre()
-    resultado.append(estado_inicial)
-    resultado.reverse()
-    print(resultado)
+    if nodo_solucion == None:
+        print("Algo falló en la función")
+    else:
+        # mostrar resultado
+        resultado = []
+        nodo = copy.copy(nodo_solucion)
+        compara = nodo.get_padre() == None
+        while nodo.get_padre() != None:
+            resultado.append(nodo.get_datos())
+            if nodo.get_padre() == None:
+                break
+            else:
+                nodo = nodo.get_padre()
+
+        resultado.append(estado_inicial)
+        resultado.reverse()
+        print(resultado)
 
 
